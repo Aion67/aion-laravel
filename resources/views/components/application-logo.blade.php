0f982b1296810e2312cde45277a110303818016d
JMinallah <joviaminallah@gmail.com>
2026-05-10 23:35:58 +0300

    feat: replace application logo with an image component in guest and welcome layouts

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,16 +1,1 @@
-<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <title>Aion Pharmacy</title>
-  <desc>A modern pharmacy logo with a green cross in a circle</desc>
-  
-  <!-- Circle background -->
-  <circle cx="50" cy="50" r="46" fill="none" stroke="currentColor" stroke-width="3" opacity="0.8"/>
-  
-  <!-- Vertical bar of cross -->
-  <rect x="36" y="18" width="28" height="64" rx="4" fill="currentColor" opacity="0.9"/>
-  
-  <!-- Horizontal bar of cross -->
-  <rect x="18" y="36" width="64" height="28" rx="4" fill="currentColor" opacity="0.9"/>
-  
-  <!-- Center square -->
-  <rect x="40" y="40" width="20" height="20" rx="3" fill="white"/>
-</svg>
+<img src="{{ asset('aion_pharmacy_logo.svg') }}" alt="Aion Pharmacy" {{ $attributes }} />
